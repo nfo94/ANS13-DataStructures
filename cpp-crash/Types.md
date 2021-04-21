d62bf7105c4ea5cf1202e568ace0475681c79b2e
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2021-04-20 22:43:52 -0300

    types and aps

---
+++ cpp-crash/Types.md
@@ -0,0 +1,27 @@
+1. Primitive Data Types: These data types are built - in or predefined data types
+   and can be used directly by the user to declare variables.example : int, char,
+   float, bool etc. Primitive data types available in C++ are :
+   Integer
+   Character
+   Boolean
+   Floating Point
+   Double Floating Point
+   Valueless or Void
+   Wide Character
+
+2. Derived Data Types : The data - types that are derived from the primitive or
+   built - in datatypes are referred to as Derived Data Types. These can be of four
+   types namely :
+   Function
+   Array
+   Pointer
+   Reference
+
+3. Abstract or User - Defined Data Types : These data types are defined by user
+   itself. Like, defining a class in C++ or a structure.C++ provides the following
+   user - defined datatypes :
+   Class
+   Structure
+   Union
+   Enumeration
+   Typedef
--- lists/APS/ArrayList.h
+++ lists/APS/ArrayList.h
@@ -1,22 +1,48 @@
 #ifndef ARRAYLIST_H
 #define ARRAYLIST_H
 
-class ArrayList
+template <class T> class ArrayList
 {
+private:
+    int m_size;
+    int m_capacity;
+    T* m_elements;
+
 public:
-    ArrayList();
-    int size();
-    boolean isEmpty();
+    ArrayList()
+    {
+        m_capacity = 5;
+        m_size = 0;
+        m_elements = new T[m_capacity];
+    };
+
+    int size()
+    {
+        return m_size;
+    };
+
+    bool isEmpty()
+    {
+        return m_size == 0;
+    };
+
     void add(T elem);
-    boolean add(T elem, int index);
-    boolean remove(T elem);
-    boolean remove(int index);
+
+    bool add(T elem, int index);
+
+    bool remove(T elem);
+
+    bool remove(int index);
+
     void clear();
+
     T get(int index);
+
     int indexOf(T elem);
+
     void print();
 
     virtual ~ArrayList();
-}
+};
 
 #endif /* ARRAYLIST_H */
