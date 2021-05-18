e0008e24515942ef38c514c2212af8e315b1d48c
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2021-05-17 22:01:21 -0300

    beginning trees

--- classroom/ArvoreDeBuscaBinaria/BinaryNode.h
+++ classroom/ArvoreDeBuscaBinaria/BinaryNode.h
@@ -1,21 +1,25 @@
 #ifndef BINARYNODE_H
-#define BINARYNODE_H
 #include <iostream>
 #include <cstdlib>
+#define BINARYNODE_H
+
 using namespace std;
-class BinaryNode {
-    public:
-        int element;
-        BinaryNode* left;
-        BinaryNode* right;
-        BinaryNode (int e,BinaryNode* l,BinaryNode* r) {
-            element=e;
-            left=l;
-            right=r;
-        }
-        ~BinaryNode() {
-            cout<<"apagando nó "<<element<<endl;
-        }
+
+class BinaryNode
+{
+public:
+    int element;
+    BinaryNode *left;
+    BinaryNode *right;
+    BinaryNode(int e, BinaryNode *l, BinaryNode *r)
+    {
+        element = e;
+        left = l;
+        right = r;
+    }
+    ~BinaryNode()
+    {
+        cout << "apagando nó " << element << endl;
+    }
 };
 #endif /* BINARYNODE_H */
-
